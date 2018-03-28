b989d6fda294d003f8fb1a6726349cc622b87f85
Luis Ricardo Benhossi <ricardo.benhossi@gmail.com>
2018-03-28 06:57:45 -0300

    New Class: List.

--- Section01-PythonIntroduction/1.6-Lists.py
+++ Section01-PythonIntroduction/1.6-Lists.py
@@ -1,1 +1,46 @@
-print('AUR. Te amo Natalia.')
+"""
+Lists are variables with an index, like an array. Use [] to set a list
+Use DIR to see all the methods this variable acceppts
+"""
+vListVar = []
+print(type(vListVar))
+
+# APPEND: add a data to your list.
+vListVar.append('Python')
+print(vListVar)
+vListVar.append('Java')
+print(vListVar)
+vListVar.append('PHP')
+vListVar.append('Delphi')
+
+# REVERSE: invert the rder off all components of the list
+print(vListVar)
+vListVar.reverse()
+print(vListVar)
+# a new revers, ofcourse returns the list to the original order.
+print(vListVar.reverse())
+
+# INSERT: inset a data in an index you want.It takes 2 parameters:
+    # the position you want to add the new data (integer started at 0)
+    # the data itself
+vListVar.insert(2, 'Go')
+print(vListVar)
+
+# POP: removes the last element of the list and return it.
+vListVar.pop()
+print(vListVar)
+
+# COUNT: returns how many times a data shows in the list
+print(vListVar.count('Go'))
+
+#REMOVE: removes a data from the list
+vListVar.remove('Java')
+print(vListVar)
+
+# A list can contain any type of variables, including another list
+vListVar.append(3)
+vListVar.append(2.8)
+print(vListVar)
+vList2 = [1, 2, 3]
+vListVar.append(vList2)
+print(vListVar)
---
+++ Section01-PythonIntroduction/BasicCommands.py
@@ -0,0 +1,15 @@
+"""
+This file contains a bunch of common and basic commands to use on Python
+"""
+
+# TYPE: shows a type of a variable
+x = "test"
+y = 2
+z = 2.9
+print(type(x))
+print(type(y))
+print(type(z))
+
+# DIR: Shows a all the mothods you can use with a variable.
+dir(str)
+dir(int)
