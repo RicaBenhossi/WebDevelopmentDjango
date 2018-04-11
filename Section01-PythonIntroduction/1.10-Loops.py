b48c666d70386e4b8db63e983dd9708814797d71
ricardo <ricardo@logusretail.com.br>
2018-04-11 17:39:10 -0300

    New loop lace

--- Section01-PythonIntroduction/1.10-Loops.py
+++ Section01-PythonIntroduction/1.10-Loops.py
@@ -10,5 +10,12 @@ for x in lang:
     print(x)
 print()
 for y in range(10):
-
     print(y)
+
+# WHILE executes unless 1 time and there is no explicit stop point
+controler = 10
+while controler >= 0:
+    print("Countdown: ", controler)
+    controler -= 1
+    if (controler < 0):
+        print("DONE!")
