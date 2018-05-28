d9330c54594d58d7698a7d5e3aa756ac760e583c
Luis Ricardo Benhossi <ricardo.benhossi@gmail.com>
2018-05-28 11:07:24 -0300

    New tests .

---
+++ PythonTesting/circle_test.py
@@ -0,0 +1,8 @@
+import unittest
+
+from circle import circle_area
+from math import pi
+
+class TestCircleArea(unittest.TestCase):
+    def teste_area(self):
+        # Test area 
---
+++ Testes.py
@@ -0,0 +1,20 @@
+
+
+d = float(input('Type the number you want divide: '))
+q = float(input('Type the coeficient of operation: '))
+try:
+    assert q > 0
+except:
+    print('Error. Devide by 0.')
+    exit(0)
+
+try:
+    total = d // q
+    remainder = d % q
+finally:
+    print('The result is: {}'.format(total))
+    if(d % q > 0):
+        print('The remainder of the operation is: {}'.format(remainder))
+
+
+# print(((225 * 100) / 197) - 100)
