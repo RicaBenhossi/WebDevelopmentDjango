c6aa7611a46c1f3513fd3e3e785dee7499d5374c
Luis Ricardo Benhossi <ricardo.benhossi@gmail.com>
2018-07-05 14:15:49 -0300

    Returning

--- BDTest/PassCryp/test.py
+++
@@ -1,8 +0,0 @@
-from simplecrypt import encrypt, decrypt
-
-
-passwd = str(input('Digite a senha: '))
-print(passwd)
-passwd_crypt = encrypt('', passwd).decode('utf-8')
-print(passwd_crypt)
-passwd_decrypt = decrypt('', passwd_crypt).decode('utf-8')
---
+++ teste_dict.py
@@ -0,0 +1,5 @@
+dictionary1 = {"teste": "iten1", "key2": 2}
+dictionary2 = {"teste": "iten1", "key2": 2}
+dictionary3 = {"teste": "iten1", "key2": 4}
+print(dictionary1 == dictionary2)
+print(dictionary1 == dictionary3)
